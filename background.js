2176f8ea7d971e2e5017468d3f6a66eed18f0705
bowenfeng-dev <bowenfeng@Bowens-MBP.verizon.net>
2015-11-18 03:20:47 -0500

    Try set interval, not working now.

--- background.js
+++ background.js
@@ -6,6 +6,15 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     ]
   };
   chrome.tabs.query(queryInfo, function(tabs) {
+    // var i = 0;
+    // var id = setInterval(function() {
+    //   if (i === tabls.length) {
+    //     clearInterval(id);
+    //   } else {
+    //     i += 1;
+    //     chrome.tabs.reload(tabs[i].id);
+    //   }
+    // }, 5000);
     _.each(tabs, function (tab) {
         chrome.tabs.reload(tab.id);
     });
